166475262d20a9ea78676171921ad726e0514f16
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-24 10:24:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - 42);
+}
+// function distanceFromHqInfeet(distance){
+//     return distanceFromHqInBlocks(distance) *264;
+// }
+
+function distanceTravelledInFeet(start , stop){
+  return Math.abs(stop- start) * 264;
+}
+function calculatesFarePrice(start, destination){
+    const totalDistance = distanceTravelledInFeet(start, destination);
+    if (totalDistance < 400) {
+      return 0;
+    }
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264;
+  }
+  
+  function distanceTravelledInFeet(start , end){
+    return Math.abs(end - start) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination){
+    const totalDistance = distanceTravelledInFeet(start, destination);
+    if (totalDistance < 400) {
+      return 0;
+    }
+    else if (totalDistance > 400 && totalDistance < 2000){
+      return (totalDistance -400) * 0.02;
+    }
+     else if (totalDistance > 2000 && totalDistance < 2500){
+      return 25;
+    }
+    else if (totalDistance > 2500){
+      return 'cannot travel that far';
+    }
+  
+  }
+
+// function calculatesFarePrice(start,stop){
+//     const totalDistance = distanceTravelledInFeet (start,stop);
+
+// } 
+//     else if (totalDistance > 400 && totalDistance < 2000){
+//     return (totalDistance -400) * 0.02;
+// }
+// else if (totalDistance > 400 && totalDistance < 2000){
+//     return (totalDistance -400) * 0.02;
+//   }
+//    else if (totalDistance > 2000 && totalDistance < 2500){
+//     return 25;
+//   }
+//   else if (totalDistance > 2500){
+//     return 'cannot travel that far';
+//   }
+
